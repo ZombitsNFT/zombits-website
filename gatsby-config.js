af62ae9577937169c7546ff3e63243291342053c
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2021-05-24 15:56:26 +1000

    Add missing plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,11 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-sass",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
